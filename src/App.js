a765885eb8be4740145a888d0e88802e32c7da5b
Homero Silva <hsilvap@me.com>
2020-10-16 19:04:29 -0500

    Add header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-import styled from "styled-components";
 import "./App.css";
+import AllProducts from "./components/AllProducts";
 import Header from "./components/Header";
+import { useQuery, gql } from "@apollo/client";
 
-const AppLayout = styled.div`
-  display: grid;
-  grid-template-rows: auto 2fr auto;
+const ALL_PRODUCTS = gql`
+  query {
+    products {
+      id
+      price(currency: USD)
+      image_url
+      title
+    }
+  }
 `;
+
 function App() {
+  const { loading, error, data } = useQuery(ALL_PRODUCTS);
   return (
     <>
       <Header />
+      <AllProducts loading={loading} error={error} data={data} />
     </>
   );
 }
---
+++ src/components/Product/Product.component.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledProduct = styled.div`
+  display: flex;
+  flex-flow: column;
+  min-height: 400px;
+  justify-content: center;
+  align-items: center;
+  > img {
+    margin: 2em;
+    max-width: 100px;
+    max-height: 100px;
+  }
+  > button {
+    background-color: #4b5648;
+    color: white;
+    padding: 1em;
+    margin: 1em;
+  }
+`;
+
+const Product = ({ id, price, image_url, title, addToCart }) => {
+  return (
+    <StyledProduct>
+      <img src={image_url} alt={title} />
+      <label>{title}</label>
+      <label>{`From ${price}`}</label>
+      <button onClick={addToCart}>Add to Cart</button>
+    </StyledProduct>
+  );
+};
+
+export default Product;
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image_url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  addToCart: PropTypes.func.isRequired,
+};
