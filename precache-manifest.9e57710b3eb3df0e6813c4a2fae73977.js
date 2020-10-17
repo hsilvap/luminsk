28e4eb8d58a6bf05a6a6d22713b75e0780a6ea83
Homero Silva <hsilvap@me.com>
2020-10-16 21:08:16 -0500

    Updates

---
+++ precache-manifest.9e57710b3eb3df0e6813c4a2fae73977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5591ebb8c1c4d9d4e5936cd46105c90e",
+    "url": "/luminsk/index.html"
+  },
+  {
+    "revision": "b82bd5aa26652a2a73b9",
+    "url": "/luminsk/static/css/main.454459b4.chunk.css"
+  },
+  {
+    "revision": "b149a1d6c730c86613f5",
+    "url": "/luminsk/static/js/2.0e1bfe9a.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/luminsk/static/js/2.0e1bfe9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b82bd5aa26652a2a73b9",
+    "url": "/luminsk/static/js/main.eb0a3a0d.chunk.js"
+  },
+  {
+    "revision": "33d4839de7f282c15323",
+    "url": "/luminsk/static/js/runtime-main.321f6bc3.js"
+  },
+  {
+    "revision": "446d380d09c679c9719b0290696c3b4e",
+    "url": "/luminsk/static/media/cart.446d380d.svg"
+  }
+]);
